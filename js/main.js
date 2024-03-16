25252938091c038cdc4f2867330b377e94dba350
Javohir Abdumannonov <javohirabdumannnov@MacBook-Pro-Javohir.local>
2024-03-16 16:33:15 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.footer-content__swipper', {
+    // Optional parameters
+    // direction: 'horizontal',
+    // loop: true,
+    // loopedSlides: 0,
+    // freeMode: true,
+  
+    // // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   type: 'fraction'
+    // },
+    // spaceBetween: 30,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.footer-swipper-elipes-1',
+      prevEl: '.footer-swipper-elipes-2',
+    },
+    mousewhel: true,
+    keyboaard: true,
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    // slidesPerView: 3,
+    // centeredSlides: true
+  });
+
